dab3f68066c0ee4ac91b4b1757f2d6a2c08bad8f
Juan Carlos Argueda <carlosargueda31@gmail.com>
2025-08-10 09:01:20 -0500

    Add: Se agrego sistema de notificaciones por api

--- frontend/src/components/dashboard/contenidochat.jsx
+++ frontend/src/components/dashboard/contenidochat.jsx
@@ -1030,7 +1030,7 @@ function Cabecerachat({ dataclic, dataclicuser }) {
                                 }
                             ],
                             "cCliente": numpromt + "@c.us",
-                            "cUsuario": "51986230303@c.us"
+                            "cUsuario": "51915197767@c.us"
                         }
                     )
                 });
