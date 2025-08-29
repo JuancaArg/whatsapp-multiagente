e274c377b94320510f539e788b15cfa5c06c96e0
Juan Carlos Argueda <carlosargueda31@gmail.com>
2025-08-29 10:25:04 -0500

    Fixed(Botone/WhatsappApi) : Se agrego reconocmiento de texto de boton

--- backend/services/Meta/EnvioMensajes.js
+++ backend/services/Meta/EnvioMensajes.js
@@ -23,111 +23,132 @@ export const EnvioMensajes = (data) => {
 }
 
 const SendTextSimple = (to,mensaje,data) => {
-    
-    return new Promise((resolve, reject) => {
-        var options = {
-        'method': 'POST',
-        'url': 'https://graph.facebook.com/v22.0/717333071471269/messages',
-        'headers': {
-            'Content-Type': ['application/json', 'application/json'],
-            'Authorization': 'Bearer '+ process.env.META_WHATSAPP_TOKEN
-        },
-        body: JSON.stringify({
-            "messaging_product": "whatsapp",
-            "recipient_type": "individual",
-            "to": to,
-            "type": "text",
-            "text": {
-            "preview_url": false,
-            "body": mensaje
-            }
-        })
 
-        };
-        request(options, async function (error, response) {
-        if (error) throw new Error(error);
-        const json = {
-            body : JSON.parse(response.body),
-            res : data
-        }
-        const req = await InsertaRegistroCollecion(process.env.FIREBASE_COLECCION_MENSAJESAPI, json , JSON.parse(response.body).messages[0].id)
+    try{
+    
+        return new Promise((resolve, reject) => {
+            var options = {
+            'method': 'POST',
+            'url': 'https://graph.facebook.com/v22.0/717333071471269/messages',
+            'headers': {
+                'Content-Type': ['application/json', 'application/json'],
+                'Authorization': 'Bearer '+ process.env.META_WHATSAPP_TOKEN
+            },
+            body: JSON.stringify({
+                "messaging_product": "whatsapp",
+                "recipient_type": "individual",
+                "to": to,
+                "type": "text",
+                "text": {
+                "preview_url": false,
+                "body": mensaje
+                }
+            })
+
+            };
+            request(options, async function (error, response) {
+            if (error) throw new Error(error);
+            const json = {
+                body : JSON.parse(response.body),
+                res : data
+            }
+            const req = await InsertaRegistroCollecion(process.env.FIREBASE_COLECCION_MENSAJESAPI, json , JSON.parse(response.body).messages[0].id)
+            });
         });
-    });
+    }catch(e){
+        console.log(e);
+    }
 }
 
 const sendImageandText = async (to, mensaje, imagen, data) => {
 
-    const type  = data.tipomensaje === 'imagen' ? 'image' : 'document';
+    try {
 
-    // Generar el payload
+        const type  = data.tipomensaje === 'imagen' ? 'image' : 'document';
 
-    const IdUpload = await ApiUploadImage(imagen);
-     
-    return new Promise((resolve, reject) => {
-        var options = {
-        'method': 'POST',
-        'url': 'https://graph.facebook.com/v22.0/717333071471269/messages',
-        'headers': {
-            'Content-Type': ['application/json', 'application/json']
-            ,'Authorization': 'Bearer '+ process.env.META_WHATSAPP_TOKEN
-        },
-        body: JSON.stringify({
-            "messaging_product": "whatsapp",
-            "recipient_type": "individual",
-            "to": to,
-            "type": type,
-            [type]: {
-            "id": JSON.parse(IdUpload).id
+        // Generar el payload
+
+        const IdUpload = await ApiUploadImage(imagen);
+        
+        return new Promise((resolve, reject) => {
+            var options = {
+            'method': 'POST',
+            'url': 'https://graph.facebook.com/v22.0/717333071471269/messages',
+            'headers': {
+                'Content-Type': ['application/json', 'application/json']
+                ,'Authorization': 'Bearer '+ process.env.META_WHATSAPP_TOKEN
+            },
+            body: JSON.stringify({
+                "messaging_product": "whatsapp",
+                "recipient_type": "individual",
+                "to": to,
+                "type": type,
+                [type]: {
+                "id": JSON.parse(IdUpload).id
+                }
+            })
+
+            };
+            request(options, async function (error, response) {
+            if (error) throw new Error(error);
+            const json = {
+                body : JSON.parse(response.body),
+                res : data
             }
-        })
 
-        };
-        request(options, async function (error, response) {
-        if (error) throw new Error(error);
-        const json = {
-            body : JSON.parse(response.body),
-            res : data
-        }
+            //console.log('resonde - ', json.body);
+            const req = await InsertaRegistroCollecion(process.env.FIREBASE_COLECCION_MENSAJESAPI, json , JSON.parse(response.body).messages[0].id)
+            });
+        });        
+    } catch (error) {
+        
+        console.log(error);
 
-        //console.log('resonde - ', json.body);
-        const req = await InsertaRegistroCollecion(process.env.FIREBASE_COLECCION_MENSAJESAPI, json , JSON.parse(response.body).messages[0].id)
-        });
-    });
+    }
+
+
 
 }
 
 const ApiUploadImage = (payload) => {
-    console.log('Iniciando ApiUploadImage');
 
-    return new Promise((resolve, reject) => {
-        const base64Data = payload.data.replace(/^data:.+;base64,/, "");
-        const buffer = Buffer.from(base64Data, "base64");
-        const filepath = 'images/' + payload.filename;
+    try{
 
-        fs.writeFileSync(filepath, buffer);
+        console.log('Iniciando ApiUploadImage');
 
-        const options = {
-            method: 'POST',
-            url: 'https://graph.facebook.com/v22.0/717333071471269/media',
-            headers: {
-                'Authorization': 'Bearer ' + process.env.META_WHATSAPP_TOKEN
-            },
-            formData: {
-                'messaging_product': 'whatsapp',
-                'file': fs.createReadStream(filepath)
-            }
-        };
+        return new Promise((resolve, reject) => {
+            const base64Data = payload.data.replace(/^data:.+;base64,/, "");
+            const buffer = Buffer.from(base64Data, "base64");
+            const filepath = 'images/' + payload.filename;
 
-        request(options, (error, response) => {
-            // Limpieza del archivo temporal
-            fs.unlinkSync(filepath);
+            fs.writeFileSync(filepath, buffer);
 
-            if (error) {
-                reject(error);
-            } else {
-                resolve(response.body); // Aquí retornas correctamente
-            }
+            const options = {
+                method: 'POST',
+                url: 'https://graph.facebook.com/v22.0/717333071471269/media',
+                headers: {
+                    'Authorization': 'Bearer ' + process.env.META_WHATSAPP_TOKEN
+                },
+                formData: {
+                    'messaging_product': 'whatsapp',
+                    'file': fs.createReadStream(filepath)
+                }
+            };
+
+            request(options, (error, response) => {
+                // Limpieza del archivo temporal
+                fs.unlinkSync(filepath);
+
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(response.body); // Aquí retornas correctamente
+                }
+            });
         });
-    });
+    }
+    catch(e){
+        console.log(e);
+    }
 };
 
