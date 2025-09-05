b9f6c5678800b9082d7bf3e3f78c6540386e8660
Juan Carlos Argueda <carlosargueda31@gmail.com>
2025-09-05 15:05:39 -0500

    Fix: Nuevos numeros bloqueados

--- frontend/src/components/dashboard/variables/env.js
+++ frontend/src/components/dashboard/variables/env.js
@@ -4,4 +4,4 @@ export const conexiones = {
     front1 : process.env.REACT_APP_FRONT1 +':'+ process.env.REACT_APP_PORT_FRONT+'/'
 }
 
-export const LineasBloqueadas = ['51994698576','51986230303','51967439174']
+export const LineasBloqueadas = ['51994698576','51986230303','51993295035']
